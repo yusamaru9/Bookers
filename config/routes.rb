e29d1eccd7a12e3ec702773a5897f067d6fdc43f
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-06-12 13:27:20 +0000

    [Add] 3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  get '/' => 'homes#top'
+  
+  resources :books
+  
+  post 'books' => 'books#create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
