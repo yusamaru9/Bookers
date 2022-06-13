ee2219e5116195592f425410e082f14898dc85ee
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-06-13 13:19:25 +0000

    [Add] 3-12-4 編集画面バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def new
+    
   end
   
   def create
@@ -7,6 +8,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -25,13 +27,17 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
+    flash[:notice] = "successfully"
     book.destroy
     redirect_to '/books'
   end
